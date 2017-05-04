bb2b2e7426bc808d215ef191c861685a2544bbc5
Yufeng Liang <yufengliang@lbl.gov>
2017-05-03 20:55:57 -0700

    Sort BSE energies

--- bse.py
+++ bse.py
@@ -23,19 +23,22 @@ def bse(xi, iscf, fscf, nocc, ixyz_list, offset = 0.0, evec = None):
     
     # BSE Hamiltonian
     H_BSE = xi[:, int(nocc) : ].T * sp.diag(fscf.eigval[ : fscf.nbnd_use]) * xi[:, int(nocc) : ].conjugate()
+    #H_BSE = xi[:, int(nocc) : ].H * sp.diag(fscf.eigval[ : fscf.nbnd_use]) * xi[:, int(nocc) : ]
 
     # Solve BSE
     E, Af = la.eig(H_BSE)
 
+    ind = sp.array(sorted(range(len(E)), key = lambda i : E[i]))
     # Make sticks
     sticks = sp.zeros((len(E), len(ixyz_list) + 2))
-    sticks[:, 0] = E.real + offset
+    sticks[:, 0] = E[ind].real + offset
+
     for i, ixyz in enumerate(ixyz_list):
 
         ixmat = sp.array([ xmat_ixyz( iscf.xmat[ib, 0, :], ixyz, evec = evec ) for ib in range(int(nocc), iscf.nbnd_use) ])
         
         # transition amplitude: A_op = sum_i Af*_i < i | O | h >
-        A_op = sp.dot(ixmat, sp.matrix(Af).conjugate())
+        A_op = sp.dot(ixmat, sp.matrix(Af).conjugate())[0, ind]
         #A_op = sp.dot(ixmat, sp.matrix(Af))
         
         sticks[:, i + 2] = sp.square(abs(A_op))
